9002423a7d7aa74840846c9aaaf195a51c03eafa
Weverton Mathias Rocha <Wevertonmathias01@gmail.com>
2026-08-20 21:00:01 -0300

    fix: adiciona configuracao de CORS no UsuarioController

--- src/main/java/com/weverton/financas_api/controller/UsuarioController.java
+++ src/main/java/com/weverton/financas_api/controller/UsuarioController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/usuarios")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 
 public class UsuarioController {
 
